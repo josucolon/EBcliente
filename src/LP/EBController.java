4f319244df697b24f3bd76de3b8c65b360b73525
julenmartin <julen.martin@opendeusto.es>
2019-05-07 11:35:26 +0200

    cambios en el controller

--- src/LP/EBController.java
+++ src/LP/EBController.java
@@ -18,56 +18,61 @@ private RMIServiceLocator rsl = null;
 //		new SwitchServerGUI(this);		
 	}
 	
-    public void newTVProgram(String acronym, String description)
-    {    	
-    	try 
-    	{
-    		// Add your code related to getting the service and requesting creation of TVProgram    		
+//    public void newTVProgram(String acronym, String description)
+//    {    	
+//    	try 
+//    	{
+//    		// Add your code related to getting the service and requesting creation of TVProgram    		
 //    		rsl.getService().newTVProgram(acronym, description);
-    	} 
-    	catch (Exception e)
-    	{
-    		System.err.println("$ Error sending new TV program: " + e.toString());
-    	}
-    }
+//    	} 
+//    	catch (Exception e)
+//    	{
+//    		System.err.println("$ Error sending new TV program: " + e.toString());
+//    	}
+//    }
     
-    public void sendMessage(String phone, String text){
-    	try
-    	{	
-    		// Add your related to getting the service and sending a message
+//    public void sendMessage(String phone, String text){
+//    	try
+//    	{	
+//    		// Add your related to getting the service and sending a message
 //    		rsl.getService().receiveMessage(phone, text);
-    		
-    	} 
-    	catch(Exception e)
-    	{
-    		System.out.println("$ Error sending a message: " + e.getMessage());
-    	}
-    }
-    
-	public void switchServer(String ip, String port, String name)
-	{
-		
-		// Add your code related to switching servers		
-		System.out.println("- Switching to server: " + ip + ":" + port + "/" + name);
-	}
+//    		
+//    	} 
+//    	catch(Exception e)
+//    	{
+//    		System.out.println("$ Error sending a message: " + e.getMessage());
+//    	}
+//    }
+//    
+//	public void switchServer(String ip, String port, String name)
+//	{
+//		
+//		// Add your code related to switching servers		
+//		System.out.println("- Switching to server: " + ip + ":" + port + "/" + name);
+//	}
   
-    public void closeTVProgram() {
+    public void buscarvuelo(String Origen, String Destinio, String fecha) {
     	//This method will be use in other Labs.
     }
     
-    public void addMaximumNumberMessagesRestriction() {
+    public void registro(String nombre, String apellido, String apellido2, String dni, String password, String pago,
+    		String modoInicio, String correo) {
     	//This method will be use in other Labs.
     }
     
-    public void addPermittedOptionsRestriction() {
+    public void InicioSesionG(String usuario, String password) {
+    	//This method will be use in other Labs.
+    }
+    
+    public void InicioSesionF(String usuario, String password) {
     	//This method will be use in other Labs.
     }
 
-    public void addForbiddenWordsRestriction() {
+    public void Visa(String nombre, int numTarj, int mes, int ano, int cvv) {
     	//This method will be use in other Labs.
     }
 
-    public void generateReport() {
+    public void payPal(String usuario, String password) {
     	//This method will be use in other Labs.
     }
     
